37b7467ba824ca35c2f0439cd7fbd7d0d5b2f9a0
Zhaoguo Zhu <idcili393939@gmail.com>
2020-07-29 21:22:21 -0400

    First Attempt

--- Transforming_Images.py
+++ Transforming_Images.py
@@ -9,7 +9,7 @@ import os
 
 
 def 32x128_grayscale():
-    directory = 'data/'
+    directory = 'words/'
     for sub1 in os.listdir(directory):
         for sub2 in os.listdir(directory + sub1 + '/'):
             for files in os.listdir(directory + sub1 + '/' + sub2 + '/'):
